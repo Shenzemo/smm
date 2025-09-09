db2805e9ebcc5d315e8e1bb0844998bfb3bb9561
MGH <84147752+Shenzemo@users.noreply.github.com>
2025-09-09 20:44:12 +0330

    Update api.js

--- my-smm-panel/netlify/functions/api.js
+++ my-smm-panel/netlify/functions/api.js
@@ -1,4 +1,4 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai/google-ai");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const MASTER_LIST_URL = "https://gist.githubusercontent.com/Shenzemo/7014871bbc721823ef28a5332740445f/raw/98b589c932a2598a484afa28a348a19986be043a/gistfile1.txt";
 const CURRENCY_URL = "https://sarfe.erfjab.com/prices";
@@ -80,6 +80,7 @@ exports.handler = async () => {
     };
   }
 };
+
 
 
 
